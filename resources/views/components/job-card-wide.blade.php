4e2cc754b12f8fffa16b1639082af21134edb524
JudeAlberca <judealberca3@gmail.com>
2024-11-16 17:43:36 +0800

    1116_Vid29_Models-relationships-testing-oh-my

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -1,44 +1,21 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Pixel Positions</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:wght@400;500;600&display=swap"
-        rel="stylesheet">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
+@props(['job'])
 
-<body class="bg-black text-white font-hanken-grotesk">
-    <div class="px-10">
-        <nav class="flex justify-between items-center py-4 border-b border-white/10">
-            <div>
-                <a href="/">
-                    <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="">
-                </a>
-            </div>
-
-            <div class="space-x-6 font-bold">
-                <a href="#">Jobs</a>
-                <a href="#">Careers</a>
-                <a href="#">Salaries</a>
-                <a href="#">Companies</a>
-            </div>
-
-            <div>
-                <a href="">Post a Job</a>
-            </div>
-        </nav>
-
-        <main class="mt-10 max-w-[986px] mx-auto">
-            {{ $slot }}
-        </main>
+<x-panel class="flex gap-x-6">
+    <div>
+        <x-employer-logo />
     </div>
 
-</body>
-</html>
+    <div class="flex-1 flex flex-col">
+        <a href="#" class="self-start text-sm text-gray-400 transition-colors duration-300">{{ $job->employer->name }}</a>
+
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800">{{ $job->title }}</h3>
+
+        <p class="text-sm text-gray-400 mt-auto">{{ $job->salary }}</p>
+    </div>
+
+    <div>
+        @foreach($job->tags as $tag)
+            <x-tag :$tag />
+        @endforeach
+    </div>
+</x-panel>
